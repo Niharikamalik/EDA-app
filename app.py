22159cbbbc68191bf96ee28dad7c1e0ab233d409
Niharika Malik <niharikamalik50@gmail.com>
2023-11-18 14:08:22 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+#from pandas_profiling import ProfileReport
+from ydata_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
+# Web App Title
+st.markdown('''
+# **The EDA App**
+
+This is the **EDA App** created in Streamlit using the **pandas-profiling** library.
+
+**Credit:** App built in `Python` + `Streamlit` by [Niharika Malik](https://medium.com/@chanin.nantasenamat))
+
+---
+''')
+import streamlit as st
+
+# Using HTML and CSS to style the Streamlit app
+st.markdown(
+    """
+    <style>
+        body {
+            background-color: blue;
+            font-family: Arial, sans-serif;
+            
+        }
+        h1 {
+            color: #1f4172;
+            text-align: left;
+            
+        }
+        p {
+            color: #333333;
+            font-size: 30px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Content with styled HTML
+st.markdown("<h1>Hello Streamlit!</h1>", unsafe_allow_html=True)
+st.markdown("<p>This is a styled Streamlit application.</p>", unsafe_allow_html=True)
+
+
+# Upload CSV data
+with st.sidebar.header('1. Upload your CSV data'):
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+    st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
+""")
+
+# Pandas Profiling Report
+if uploaded_file is not None:
+    @st.cache_data
+    def load_csv():
+        csv = pd.read_csv(uploaded_file)
+        return csv
+    df = load_csv()
+    pr = ProfileReport(df, explorative=True)
+    st.header('**Input DataFrame**')
+    st.write(df)
+    st.write('---')
+    st.header('**Pandas Profiling Report**')
+    st_profile_report(pr)
+else:
+    st.info('Awaiting for CSV file to be uploaded.')
+    if st.button('Press to use Example Dataset'):
+        # Example data
+        @st.cache_data
+        def load_data():
+            a = pd.DataFrame(
+                np.random.rand(100, 5),
+                columns=['a', 'b', 'c', 'd', 'e']
+            )
+            return a
+        df = load_data()
+        pr = ProfileReport(df, explorative=True)
+        st.header('**Input DataFrame**')
+        st.write(df)
+        st.write('---')
+        st.header('**Pandas Profiling Report**')
+        st_profile_report(pr)
